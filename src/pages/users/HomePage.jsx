b32d89cacd2180bb14bcdbae88985875848156aa
Ahmed Numan <reduanahmed773@gmail.com>
2025-02-19 19:26:47 +0600

    slider and custom section 'announcement' added

--- src/pages/users/HomePage.jsx
+++ src/pages/users/HomePage.jsx
@@ -1,9 +1,20 @@
 // import { useContext } from "react";
 // import { AuthContext } from "../../contexts/contexts";
+import Slider from "@/components/Slider";
+import Announcements from "@/components/shared/Announcements";
+import TopScholarship from "@/components/TopScholarship";
 
 const HomePage = () => {
   // const { name } = useContext(AuthContext);
-  return <></>;
+  return (
+    <>
+      <header>
+        <Slider />
+        <Announcements />
+      </header>
+      <TopScholarship />
+    </>
+  );
 };
 
 export default HomePage;
