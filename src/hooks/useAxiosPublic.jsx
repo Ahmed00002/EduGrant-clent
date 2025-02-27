4f24541e46d2d67b4318e9cd43ac70a665955d58
Ahmed Numan <reduanahmed773@gmail.com>
2025-02-27 16:46:33 +0600

    application cancel features added on user application page

--- src/hooks/useAxiosPublic.jsx
+++ src/hooks/useAxiosPublic.jsx
@@ -2,8 +2,8 @@ import axios from "axios";
 
 const useAxiosPublic = () => {
   const axiosPublic = axios.create({
-    // baseURL: "https://scholarships-server.vercel.app/apis/",
-    baseURL: "http://localhost:5000/apis/",
+    baseURL: "https://scholarships-server.vercel.app/apis/",
+    // baseURL: "http://localhost:5000/apis/",
   });
   return axiosPublic;
 };
--- src/hooks/useAxiosSecure.jsx
+++ src/hooks/useAxiosSecure.jsx
@@ -3,8 +3,8 @@ import axios from "axios";
 // import useAuth from "@/hooks/useAuth";
 
 const axiosSecure = axios.create({
-  // baseURL: "https://scholarships-server.vercel.app/apis/",
-  baseURL: "http://localhost:5000/apis/",
+  baseURL: "https://scholarships-server.vercel.app/apis/",
+  // baseURL: "http://localhost:5000/apis/",
   headers: {
     Authorization: `Bearer ${localStorage.getItem("accessToken")}`,
   },
