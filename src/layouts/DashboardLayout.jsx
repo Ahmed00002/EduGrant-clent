68a9ee7b0a24cdf1092c3e0c9b07909999feb14d
Ahmed Numan <reduanahmed773@gmail.com>
2025-03-15 04:28:59 +0600

    search bar, notification badge and profile pic with status added to the dashboard navbar

--- src/layouts/DashboardLayout.jsx
+++ src/layouts/DashboardLayout.jsx
@@ -1,9 +1,16 @@
 import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
 import { AppSidebar } from "@/components/app-sidebar";
 import { Outlet, useLocation } from "react-router";
+import { Bell, Search } from "lucide-react";
+import { Avatar, AvatarFallback, AvatarImage } from "@radix-ui/react-avatar";
+import useAuth from "@/hooks/useAuth";
+import useUserRole from "@/hooks/useUserRole";
+import { Badge } from "@/components/ui/badge";
 
 const DashboardLayout = () => {
-  const location = useLocation();
+  // const location = useLocation();
+  const { user } = useAuth();
+  const { role } = useUserRole();
 
   return (
     <>
@@ -14,11 +21,51 @@ const DashboardLayout = () => {
 
           {/* Main Content */}
           <main className="flex-1 w-full ">
-            <div className="flex items-center sticky top-0 z-50 bg-white shadow-md p-4 mx-4 mt-4 rounded-lg">
+            <div className="flex items-center gap-2 sticky top-0 z-50 p-2 md:p-3 rounded-lg bg-PrimaryContent">
               <SidebarTrigger />
-              <h2 className="uppercase">
-                Dashboard/{location.pathname.split("/")[2]}
-              </h2>
+              {/* menus */}
+              <div className="flex items-center justify-between w-full">
+                <form>
+                  <div className="flex items-center gap-2 border px-2 text-gray-500 rounded-full max-w-">
+                    <Search size={15} />
+                    <input
+                      className="text-sm py-2 ring-0 outline-0"
+                      type="text"
+                      placeholder="Search"
+                    />
+                  </div>
+                </form>
+                {/* notification */}
+                <div className="flex items-center gap-4">
+                  <div className="relative z-0">
+                    <Bell className="cursor-pointer text-Primary" size={20} />
+                    <div className="size-4 bg-red-500 flex items-center justify-center text-xs text-white rounded-full absolute -top-2 -right-1">
+                      0
+                    </div>
+                  </div>
+
+                  {/* avatar */}
+                  <Avatar>
+                    <AvatarImage
+                      className="rounded-full size-9 cursor-pointer"
+                      src={user.photoURL}
+                    />
+                    <AvatarFallback>DP</AvatarFallback>
+                  </Avatar>
+                  <div>
+                    <h1 className="text-sm text-text font-medium">
+                      {user.displayName}
+                    </h1>
+                    <p className={"text-xs text-text"}>
+                      {role.isAdmin
+                        ? "Admin"
+                        : role.isModerator
+                        ? "Moderator"
+                        : ""}
+                    </p>
+                  </div>
+                </div>
+              </div>
             </div>
             <div className="p-4">
               <Outlet />
--- src/pages/admin/Stats.jsx
+++ src/pages/admin/Stats.jsx
@@ -15,7 +15,7 @@ const Stats = () => {
 
   return (
     <>
-      <section className="  rounded-lg p-6 font-inter">
+      <section className="  rounded-lg font-inter">
         {/* <h1 className="mb-6 text-3xl font-bold">Statistics</h1> */}
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
           {/* Total Users */}
